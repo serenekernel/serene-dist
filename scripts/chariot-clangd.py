e145fc9b60030731eb262ae97eb8121d6a6b4582
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-01-13 07:33:54 +1100

    chore: clangd script

---
+++ scripts/chariot-clangd.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3 
+
+import os
+import subprocess
+import sys
+from os.path import abspath, dirname
+
+project_path = dirname(dirname(abspath(__file__)))
+recipe_source_path = os.getcwd()
+
+if len(sys.argv) < 3:
+    print("Usage: chariot_clangd.py <package> <source name>", file=sys.stderr)
+    sys.exit(1)
+
+mappings = [f"{recipe_source_path}=$SOURCES_DIR/{sys.argv[2]}"]
+
+result = subprocess.run(
+    [
+        "chariot",
+        "--config",
+        project_path + "/config.chariot",
+        "--no-lockfile",
+        "exec",
+        "--rw",
+        "--recipe-context",
+        sys.argv[1],
+        "-p",
+        "clangd",
+        "-e",
+        "HOME=/root/clangd",
+        "-e",
+        "XDG_CACHE_HOME=/root/clangd/cache",
+        "-m",
+        recipe_source_path + "=" + "/chariot/sources/" + sys.argv[2] + ":ro",
+        f"clangd --background-index --clang-tidy --path-mappings {','.join(mappings)}",
+    ]
+)
